9737fd472fc55bbac3ad14d0ee34ec201e9e0e6f
Scrarf <goshikogiorgi@gmail.com>
2025-12-29 01:31:16 +0400

    first simulation prototype working

--- scripts/run.py
+++ scripts/run.py
@@ -1,16 +1,86 @@
 import os
 from CSXCAD import ContinuousStructure
+import openEMS
+import math
+from openEMS.physical_constants import C0
 
-CSX = ContinuousStructure()
+engine_unit = 1 #openEMS still works in meeters
+mm = 1e-3 #milimeter for muliplication
 
-material = CSX.AddMaterial('copper')
+f_min = 100e6  # post-processing only, not used in simulation
+f_max = 10e9   # determines mesh size and excitation signal bandwidth
+epsilon_r = 1
+v = C0 / math.sqrt(epsilon_r)
+wavelength = v / f_max 
+res = wavelength / 3e2
+# use a smaller cell size around metal edge, not just the base mesh size
+highres = res / 1.5
 
+z0 = 50 #port impedence
+
+csx = ContinuousStructure()
+
+
+
+material = csx.AddMaterial('copper')
 stl_filename = '../stl/Traces_reduced.stl'
 stl_reader = material.AddPolyhedronReader(stl_filename)
-
-    
-xml_filename = 'geometry.xml'
-CSX.Write2XML(xml_filename)
-os.system('AppCSXCAD geometry.xml')
+stl_reader.ReadFile()
 
 
+mesh = csx.GetGrid()
+mesh.SetDeltaUnit(engine_unit)
+
+
+fdtd = openEMS.openEMS()
+fdtd.SetCSX(csx)
+
+print(f"STL bounds: {stl_reader.GetBoundBox()}")
+
+mesh.AddLine('x', [86.5252 *mm, 106.401 *mm])
+mesh.AddLine('y', [-75.918 *mm, -88.0904 *mm])
+mesh.AddLine('z', [0 *mm, 1 *mm])
+
+mesh.AddLine('z', [0.4316 *mm, 0.4468 *mm, 0.5996 *mm, 0.6148 *mm]) # mesh lines for highres
+
+mesh.SmoothMeshLines('x', res)
+mesh.SmoothMeshLines('y', res)
+mesh.SmoothMeshLines('z', res)
+
+dump = csx.AddDump("curl_H_upper", dump_type=3, file_type=1)
+dump.AddBox(start=[86.5252 *mm, -75.918 *mm, 0 *mm],
+            stop=[106.401 *mm, -88.0904 *mm, 1 *mm])
+
+
+port = [None, None, None, None]
+
+port[0] = fdtd.AddLumpedPort(1, z0,
+                [0.102895, -0.077755, 0.000443],
+                [0.103505, -0.077145, 0.000607],
+                'z', excite=1)
+port[1] = fdtd.AddLumpedPort(2, z0,
+                [0.102895, -0.079355, 0.000443],
+                [0.103505, -0.078745, 0.000607],
+                'z', excite=0)
+port[2] = fdtd.AddLumpedPort(3, z0,
+                [0.090695, -0.084405, 0.000443],
+                [0.091305, -0.083795, 0.000607],
+                'z', excite=0)
+port[3] = fdtd.AddLumpedPort(4, z0,
+                [0.088295, -0.084405, 0.000443],
+                [0.088905, -0.083795, 0.000607],
+                'z', excite=0)
+
+# view geometry
+csx.Write2XML('geometry.xml')
+#os.system('AppCSXCAD geometry.xml')
+
+
+
+fdtd.SetGaussExcite(f_max / 2, f_max / 2)
+fdtd.SetBoundaryCond(["PML_8", "PML_8", "PML_8", "PML_8", "PML_8", "PML_8"])
+
+sim_path = os.path.join(os.getcwd(), 'sim')
+os.makedirs(sim_path, exist_ok=True)
+
+fdtd.Run(sim_path)
